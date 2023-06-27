272d6cd6686754d4f2053422ff31801cbbf14b62
elenaquintela <elenaquintela66@gmail.com>
2023-06-27 15:57:52 +0200

    nuevo botón github

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -33,16 +33,24 @@ export function Projects() {
       <div className="grid grid-cols-4 gap-4">
         {
           data.map(item => (
-            <div key={item.idDoc} className="bg-stone-300 p-4 rounded-lg hover:bg-blue-200 hover:text-stone-800">
-              <div className="font-bold flex justify-between">
+            <div key={item.idDoc} className="bg-stone-300 p-5 rounded-lg hover:bg-blue-200 hover:text-stone-800">
+              <div className="font-bold flex justify-between text-xl mb-3">
                 <div>{item?.titulo}</div>
                 {uid && <button onClick={() => handleDelete(item.idDoc)} className="ml-3">
                   <img src="./public/delete.svg" alt="eliminar" className="w-6 h-6" />
                 </button>}
               </div>
-              <div>{item?.descripcion}</div>
-              <br />
-              <a className='bg-slate-800 p-2 rounded-lg text-white font-semibold' href={item?.github} rel='noreferrer' target='_blank'>Ver en GitHub</a>
+              <div className='mb-4'>{item?.descripcion}</div>
+
+              <a href={item?.github} rel='noreferrer' target='_blank' className="relative inline-block text-lg group">
+                <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+                  <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+                  <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+                  <span className="relative text-base">Ver en GitHub</span>
+                </span>
+                <span className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0" data-rounded="rounded-lg"></span>
+              </a>
+
             </div>
           ))
         }
